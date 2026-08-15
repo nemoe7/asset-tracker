f844740ac7997e95003718a918d4857e77a3b9d3
nemo / stan <stanpai78@gmail.com>
2026-08-15 17:26:23 +0800

    refactor(db): add helper to initialize database connection with row factory

--- db.py
+++ db.py
@@ -8,13 +8,20 @@ SCHEMA_PATH = Path("database/schema.sql")
 def init_db():
   DB_PATH.parent.mkdir(parents=True, exist_ok=True)
 
-  connection = sqlite3.connect(DB_PATH)
-  connection.execute("PRAGMA foreign_keys = ON")
+  connection = get_db()
 
   with SCHEMA_PATH.open() as file:
     connection.executescript(file.read())
 
   connection.close()
+
+
+def get_db():
+  connection = sqlite3.connect(DB_PATH)
+  connection.row_factory = sqlite3.Row
+  connection.execute("PRAGMA foreign_keys = ON")
+
+  return connection
 
 
 if __name__ == "__main__":
