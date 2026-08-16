2d82a3629d4bb48ec68c550cf008359fb8c1c993
nemo / stan <stanpai78@gmail.com>
2026-08-16 13:31:01 +0800

    feat(docker): upgrade zrok service and configure agent

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-import config
 from app import create_app
 
 app = create_app()
@@ -7,6 +6,6 @@ app = create_app()
 if __name__ == "__main__":
   app.run(
     host="0.0.0.0",
-    port=config.PORT,
+    port=5000,
     debug=True,
   )
